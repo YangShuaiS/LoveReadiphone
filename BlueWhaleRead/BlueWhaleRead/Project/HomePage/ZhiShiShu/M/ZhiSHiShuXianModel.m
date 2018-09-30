da4f45fb90ddc6a3d31ddda7d09b554be2316651
YangShuai <yangshuai@zhiyang-media.com>
2018-09-30 10:32:05 +0800

    1

---
+++ BlueWhaleRead/BlueWhaleRead/Project/HomePage/ZhiShiShu/M/ZhiSHiShuXianModel.m
@@ -0,0 +1,13 @@
+//
+//  ZhiSHiShuXianModel.m
+//  BlueWhaleRead
+//
+//  Created by 杨帅 on 2018/9/29.
+//  Copyright © 2018年 YS. All rights reserved.
+//
+
+#import "ZhiSHiShuXianModel.h"
+
+@implementation ZhiSHiShuXianModel
+
+@end
---
+++ BlueWhaleRead/BlueWhaleRead/Project/HomePage/ZhiShiShu/M/ZhiShiShuModel.m
@@ -0,0 +1,17 @@
+//
+//  ZhiShiShuModel.m
+//  BlueWhaleRead
+//
+//  Created by 杨帅 on 2018/9/29.
+//  Copyright © 2018年 YS. All rights reserved.
+//
+
+#import "ZhiShiShuModel.h"
+
+@implementation ZhiShiShuModel
++ (void)InitializeModel{
+    [ZhiShiShuDataModel mj_setupObjectClassInArray:^NSDictionary *{
+        return @{@"point":@"ZhiShiShuNeiRongModel",@"relation":@"ZhiSHiShuXianModel"};
+    }];
+}
+@end
